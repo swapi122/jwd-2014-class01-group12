e68640aee47c9ce6e8f0d4efa1dd17b99c2f5d25
nmti20 <nmti20@92adc72e-1d15-0d8f-fea6-0d13d69c7c17>
2014-09-10 15:12:47 +0000

    Share project "HelloWorld" into "https://jwd-2014-class01-group12.googlecode.com/svn"

---
+++ trunk/HelloWorld/src/hcmute/edu/vn/HelloWorld.java
@@ -0,0 +1,12 @@
+package hcmute.edu.vn;
+/**
+ * @author Group 05 - OOAD_Saturday
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
